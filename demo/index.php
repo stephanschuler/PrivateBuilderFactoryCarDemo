afdf2ab98eac499fc825fe553f230352830ecab3
Stephan Schuler <stephan.schuler@netlogix.de>
2018-05-26 05:35:44 +0200

    Add demo/index.php

---
+++ demo/index.php
@@ -0,0 +1,42 @@
+<?php
+
+use Composer\Autoload\ClassLoader;
+use StephanSchuler\PrivateCarConstructorDemo\Builder\CarBuilder;
+use StephanSchuler\PrivateCarConstructorDemo\Model\Car;
+
+/** @var ClassLoader $autoloader */
+$autoloader = require_once __DIR__ . '/../vendor/autoload.php';
+
+$car1 = Car::build(function (CarBuilder $car) {
+    /*
+     * This is not an actual car but only a list of parts. The car object is created
+     * automatically just after this closure returns.
+     */
+    $car
+        ->setEngine(300, 350)
+        ->addWheel(20)
+        ->addWheel(20)
+        ->addWheel(21)
+        ->addWheel(21);
+});
+
+$car2 = Car::build(function (CarBuilder $car) {
+    /*
+     * This is not an actual car but only a list of parts. The car object is created
+     * automatically just after this closure returns.
+     */
+    $car
+        ->addWheel(16)
+        ->addWheel(16)
+        ->addWheel(16)
+        ->addWheel(16)
+        ->setEngine(180, 90);
+});
+
+printf(
+    '<pre>%s</pre>',
+    json_encode([
+        $car1,
+        $car2
+    ])
+);
